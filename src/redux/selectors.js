91ca47038b37ea579257499d3c2c88af05cd5ba8
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-29 10:20:31 +0300

    comment

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,6 @@
 export const getContacts = state => state.reducer.items;
 export const getFilter = state => state.reducer.filter;
+
 //композитний / складний селектор selector
 export const getFilteredContacts = state => {
   const contacts = getContacts(state);
@@ -10,3 +11,18 @@ export const getFilteredContacts = state => {
     name.toLowerCase().includes(normalizedFilter),
   );
 };
+
+// мемоізація (в нашому випадку не потрібна)
+
+// import { createSelector } from '@reduxjs/toolkit';
+//
+// export const getFilteredContacts = createSelector(
+//   [getContacts, getFilter],
+//   (contacts, filter) => {
+//     const normalizedFilter = filter.toLowerCase();
+//
+//     return contacts.filter(({ name }) =>
+//       name.toLowerCase().includes(normalizedFilter),
+//     );
+//   },
+// );
